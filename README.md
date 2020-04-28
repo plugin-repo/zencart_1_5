58f8385528cbcd5e4ea118d83f2540775b82b8af
Plugins <haresh.panchal@paymentz.com>
2020-04-28 18:13:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# CMS-ZenCart version 1.5 Installation guide
+1.) Goto your ZenCart root directory and paste the “includes” folder in it.
+
+2.) Login to your ZenCart admin panel, goto tab Modules=>Payment and locate "CMS Zencart".
+
+3.) Fill all the details provided by cms_zencart and click on the upadate button.
+
+4.) Now, go to your website [front end office] to check Payment method on checkout page.
+
+
---
+++ includes/templates/responsive_classic/templates/tpl_checkout_canceled_default.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Page Template
+ *
+ * @package templateSystem
+ * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: tpl_page_3_default.php 3464 2006-04-19 00:07:26Z ajeh $
+ */
+?>
+<div class="centerColumn" id="pageThree">
+<h1 id="pageThreeHeading"><?php echo HEADING_TITLE; ?></h1>
+
+
+
+<div class="buttonRow back"><?php echo zen_back_link() . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>'; ?></div>
+</div>
